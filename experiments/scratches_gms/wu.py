ee8073f9289d3c91bbafb52934c1c62efe68d657
zgbkdlm <zgbkdlm@gmail.com>
2024-11-20 10:48:58 +0100

    wu

--- experiments/scratches_gms/wu.py
+++ experiments/scratches_gms/wu.py
@@ -5,9 +5,10 @@ from gfk.synthetic_targets import make_gaussian_mixture, gm_lin_posterior
 from gfk.tools import logpdf_gm, sampling_gm
 from gfk.feynman_kac import make_fk_wu_normal
 from gfk.resampling import stratified
+from gfk.experiments import generate_gm
 
 jax.config.update("jax_enable_x64", True)
-key = jax.random.PRNGKey(1)
+key = jax.random.PRNGKey(666)
 
 # Define the forward process
 a, b = -0.5, 1.
@@ -24,26 +25,15 @@ dt = T / nsteps
 ts = jnp.linspace(0., T, nsteps + 1)
 
 # Define the data
-dim = 2
-ws = jnp.array([0.1, 0.5, 0.2, 0.2])
-ms = jnp.array([[2., 2.],
-                [2., -2.],
-                [-2., -2.],
-                [-2., 2.]])
-c1_ = jnp.array([[1., 0.2], [0.2, 0.5]])
-c2_ = jnp.array([[1., -0.2], [-0.2, 0.5]])
-c3_ = jnp.array([[0.5, 0.2], [0.2, 1.]])
-c4_ = jnp.array([[2., 0.], [0., 1.]])
-covs = jnp.concatenate([c1_[jnp.newaxis], c2_[jnp.newaxis], c3_[jnp.newaxis], c4_[jnp.newaxis]]) * 0.1
+key, subkey = jax.random.split(key)
+dx, dy = 10, 1
+ncomponents = 5
+ws, ms, covs, obs_op, obs_cov = generate_gm(subkey, dx, dy, ncomponents)
 eigvals, eigvecs = jnp.linalg.eigh(covs)
 wTs, mTs, eigvalTs, score, rev_drift, rev_dispersion = make_gaussian_mixture(ws, ms, eigvals, eigvecs, a, b, t0, T)
 
 # Define the observation operator and the observation covariance
-key, subkey = jax.random.split(key)
-obs_op = jax.random.uniform(subkey, (1, dim))
-obs_cov = jnp.eye(1)
-
-y = jnp.ones(1)
+y = jnp.zeros(dy)
 posterior_ws, posterior_ms, posterior_covs = gm_lin_posterior(y, obs_op, obs_cov, ws, ms, covs)
 posterior_eigvals, posterior_eigvecs = jnp.linalg.eigh(posterior_covs)
 
@@ -64,7 +54,7 @@ smc_sampler = make_fk_wu_normal(obs_op, obs_cov,
                                 fwd_drift, score,
                                 ts, y, langevin_step_size,
                                 mode='guided',
-                                proposal='langevin')
+                                proposal='direct')
 
 # samples usT, weights log_wsT, and effective sample sizes esss
 key, subkey = jax.random.split(key)
@@ -75,12 +65,12 @@ keys = jax.random.split(subkey, num=256)
 post_samples = jax.vmap(sampling_gm, in_axes=[0, None, None, None, None])(keys, posterior_ws, posterior_ms,
                                                                           eigvalTs, eigvecs)
 
-grid = jnp.linspace(-8, 8, 1000)
-meshgrid = jnp.meshgrid(grid, grid)
-cartesian = jnp.dstack(meshgrid)
-logpdf = lambda x_: logpdf_gm(x_, posterior_ws, posterior_ms, posterior_covs)
-pdfs = jax.vmap(jax.vmap(logpdf))(cartesian)
-plt.contour(*meshgrid, pdfs, levels=50)
+# grid = jnp.linspace(-8, 8, 1000)
+# meshgrid = jnp.meshgrid(grid, grid)
+# cartesian = jnp.dstack(meshgrid)
+# logpdf = lambda x_: logpdf_gm(x_, posterior_ws, posterior_ms, posterior_covs)
+# pdfs = jax.vmap(jax.vmap(logpdf))(cartesian)
+# plt.pcolormesh(*meshgrid, pdfs)
 
 plt.scatter(usT[:, 0], usT[:, 1], s=1)
 plt.scatter(post_samples[:, 0], post_samples[:, 1], facecolors='none', edgecolors='tab:red', s=20)
---
+++ gfk/experiments.py
@@ -0,0 +1,23 @@
+"""
+This contains the common experiment settings.
+"""
+import jax
+import jax.numpy as jnp
+
+
+def generate_gm(key, dx, dy, ncomponents):
+    """Generate a GM (with observation) model.
+    """
+    key_ws, key_ms, key_covs, key_obs_op, key_obs_cov = jax.random.split(key, num=5)
+    ws = jax.random.beta(key_ws, a=3., b=3., shape=(ncomponents,))
+    ws = ws / jnp.sum(ws)
+
+    ms = jax.random.uniform(key_ms, minval=-5., maxval=5., shape=(ncomponents, dx))
+
+    cov_rnds = jax.random.normal(key_covs, shape=(ncomponents, dx))
+    covs = (jnp.einsum('...i,...j->...ij', cov_rnds, cov_rnds) + jnp.eye(dx)[None, :, :]) * 0.1
+
+    obs_op = jax.random.normal(key_obs_op, shape=(dy, dx))
+    obs_cov_rnds = jax.random.normal(key_covs, shape=(dy, ))
+    obs_cov = (jnp.outer(obs_cov_rnds, obs_cov_rnds) + jnp.eye(dy)) * 1.
+    return ws, ms, covs, obs_op, obs_cov
