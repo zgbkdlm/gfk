60532e6d87ddb96e6447c5ec9e9063653155a518
zgbkdlm <zgbkdlm@gmail.com>
2024-11-22 09:24:58 +0100

    add truth for comparison

--- gfk/likelihoods.py
+++ gfk/likelihoods.py
@@ -95,12 +95,21 @@ def pushfwd_normal_batch(obs_op: Array,
     return Fs, omegas
 
 
-def pushfwd_gibbs():
-    pass
+def true_pushfwd_normal(y, x, t, m, v, obs_op, obs_cov):
+    dy, dx = obs_op.shape
+    fwd_op, fwd_var = jnp.exp(-0.5 * t), 1 - jnp.exp(-t)
+    my, vy = obs_op @ m, obs_op @ v @ obs_op.T + obs_cov
+    cov_xy = v @ obs_op.T
 
+    mk, vk = m * fwd_op, fwd_op ** 2 * v + jnp.eye(dx) * fwd_var
+    myk, vyk = my * fwd_op, fwd_op ** 2 * vy + jnp.eye(dy) * fwd_var
+    cov_xyk = fwd_op ** 2 * cov_xy
+    cov_yxk = cov_xyk.T
+    chol = jax.scipy.linalg.cho_factor(vk)
 
-def bridge():
-    pass
+    cond_m = myk + cov_yxk @ jax.scipy.linalg.cho_solve(chol, x - mk)
+    cond_v = vyk - cov_yxk @ jax.scipy.linalg.cho_solve(chol, cov_xyk)
+    return jax.scipy.stats.multivariate_normal.logpdf(y, cond_m, cond_v)
 
 
 def bridge_log_likelihood(ref_ll: Callable[[Array, Array], FloatScalar],
--- tests/test_likelihoods.py
+++ tests/test_likelihoods.py
@@ -1,7 +1,7 @@
 import jax
 import jax.numpy as jnp
 import numpy.testing as npt
-from gfk.likelihoods import pushfwd_normal, pushfwd_normal_batch
+from gfk.likelihoods import pushfwd_normal, pushfwd_normal_batch, true_pushfwd_normal
 
 jax.config.update("jax_enable_x64", True)
 
@@ -45,3 +45,30 @@ def test_gaussian():
                 1. - jnp.exp(-(ts[i] - ts[0])))
         npt.assert_allclose(F, true_F)
         npt.assert_allclose(Omega, true_Omega)
+
+
+def test_true_pushfwd_normal():
+    key = jax.random.PRNGKey(666)
+    m = jnp.array([1.1, 2.1])
+    v = jnp.array([[1., 0.2],
+                   [0.2, 1.5]])
+    obs_op = jax.random.normal(key, (3, 2))
+    obs_cov = jnp.array([[1., 0.1, -0.1],
+                         [0.1, 2., 0.2],
+                         [-0.1, 0.2, 1.7]])
+
+    @jax.jit
+    def ll(y_, x_, t):
+        return true_pushfwd_normal(y_, x_, t, m, v, obs_op, obs_cov)
+
+    # The likelihood function should coincide at 0
+    key, subkey = jax.random.split(key)
+    y = jax.random.normal(subkey, (3,))
+    key, subkey = jax.random.split(key)
+    x = jax.random.normal(subkey, (2,))
+
+    npt.assert_allclose(ll(y, x, 0.), jax.scipy.stats.multivariate_normal.logpdf(y, obs_op @ x, obs_cov), rtol=1e-10)
+
+    # The likelihood should become whittened as t -> inf
+    npt.assert_allclose(ll(y, x, 20.), jax.scipy.stats.multivariate_normal.logpdf(y, jnp.zeros(3), jnp.eye(3)),
+                        rtol=1e-4)
