b71714e70184013e7545b4d7c600ba960108c96e
zgbkdlm <zgbkdlm@gmail.com>
2025-05-23 11:20:29 +0200

    ed

--- experiments/summary/tabulate_gms.py
+++ experiments/summary/tabulate_gms.py
@@ -27,20 +27,22 @@ for method in methods:
 
 print('===========================')
 methods = ['aux-noiseless', 'mcgdiff']
+nparticless = [1024, 4096, 16384]
 for method in methods:
     for dx in dxs:
-        prefix = method + f'-{dx}-{nparticles}-0.0'
+        for nparticles in nparticless:
+            prefix = method + f'-{dx}-{nparticles}-0.0'
 
-        swds = np.zeros(nmcs)
-        esss = np.zeros(nmcs)
-        for k in range(nmcs):
-            filename = prefix + f'-{k}.npz'
+            swds = np.zeros(nmcs)
+            esss = np.zeros(nmcs)
+            for k in range(nmcs):
+                filename = prefix + f'-{k}.npz'
 
-            swds[k] = np.load(f'./results/gms/{filename}')['swd']
-            esss[k] = np.mean(np.load(f'./results/gms/{filename}')['esss'])
+                swds[k] = np.load(f'./results/gms/{filename}')['swd']
+                esss[k] = np.mean(np.load(f'./results/gms/{filename}')['esss'])
 
-        mean = np.mean(swds)
-        std = np.std(swds)
+            mean = np.mean(swds)
+            std = np.std(swds)
 
-        print(f'Method {method} | Dim / nparticles {dx} / {nparticles} '
-              f'| Mean: {mean:.4f} | Std: {std:.4f} | ESS {np.mean(esss):.1f}')
+            print(f'Method {method} | Dim / nparticles {dx} / {nparticles} '
+                  f'| Mean: {mean:.4f} | Std: {std:.4f} | ESS {np.mean(esss):.1f}')
